1e9d9a4bf1a5b8b02c4333423246e072f4c6dfe7
ayotunde obasa <ayobam222@gmail.com>
2023-01-11 06:43:35 -0800

    style: further update to landing page

--- app/page.tsx
+++ app/page.tsx
@@ -4,17 +4,22 @@ import React from "react";
 const page = () => {
   return (
     <div className="flex items-center justify-center min-h-screen">
-      <div className="grid gap-4">
-        <h2 className="text-6xl font-bold">Oops!!🤭🤭🤭🤭</h2>
+      <div className="flex flex-col items-center space-y-4">
+        <h2 className="text-4xl font-bold lg:text-6xl">Oops!!🤭🤭🤭🤭</h2>
         <p className="max-w-lg text-base font-medium text-center text-gray-500 ">
           This assessment is directed to the settings page and you my
           friend🤗🤗🫂🫂 are not on the settings page
         </p>
-        <p>
+        <p className="max-w-lg text-base font-medium text-center text-purple-500">
           To get to the settings PAGE, you want to either press the button🔽🔽🔽
           below or use the sidebar
         </p>
-        <Link href="/settings">Go to the Settings page</Link>
+        <Link
+          href="/settings"
+          className="px-8 py-4 text-lg text-white bg-black border rounded-xl border-gray-50 w-max "
+        >
+          Go to the Settings page
+        </Link>
       </div>
     </div>
   );
